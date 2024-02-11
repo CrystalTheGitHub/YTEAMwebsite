620fe2a4dd090209cbea0f2c0e58c78199a2eca8
CrystalTheGitHub <152086195+CrystalTheGitHub@users.noreply.github.com>
2024-02-11 11:01:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const news = document.getElementById('ras')
+const ni = document.getElementById('ras2')
+const x = document.getElementById('hih')
+news.onclick = function(){
+    ni.className = 'rassnov22'
+    news.className = 'rassnov2'
+}
+hih.onclick = function (){
+    ni.className = 'rassnov2'
+    news.className = 'rassnov'
+}
