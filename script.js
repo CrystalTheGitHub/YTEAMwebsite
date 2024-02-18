17cb8583b3c64a2ddf4db8e8bcc4911f4c351aa5
CrystalTheGitHub <152086195+CrystalTheGitHub@users.noreply.github.com>
2024-02-18 20:44:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const news = document.getElementById('ras')
 const ni = document.getElementById('ras2')
 const x = document.getElementById('hih')
+const o = document.getElementById('o')
 news.onclick = function(){
     ni.className = 'rassnov22'
     news.className = 'rassnov2'
@@ -8,4 +9,9 @@ news.onclick = function(){
 hih.onclick = function (){
     ni.className = 'rassnov2'
     news.className = 'rassnov'
+}
+o.onclick = function (){
+    ni.className = 'rassnov2'
+    news.className = 'rassnov'
+    alert('Спасибо, что прошли форму. Нажмите ОК, чтобы продолжить.');
 }
